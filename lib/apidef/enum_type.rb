e9f5af5a29ad424ae3e0fb52373901df2dbc29a2
André Diego Piske <andrepiske@gmail.com>
2021-10-23 09:38:14 +0200

    Fix enum type in Apidef

--- lib/apidef/enum_type.rb
+++ lib/apidef/enum_type.rb
@@ -4,9 +4,14 @@ class Apidef::EnumType < Apidef::BaseType
   def initialize(context, attr_defn)
     super(context)
 
-    unless @enum_options = attr_defn["enum_options"]
-      raise "When type is 'enum', 'enum_options' must also be present"
+    @enum_map = attr_defn["enum_map"]
+    enum_options = attr_defn["enum_options"]
+
+    if [@enum_map, enum_options].compact.length != 1
+      raise "When type is 'enum', either 'enum_options' or 'enum_map' (but not both) must be present"
     end
+
+    @enum_options = enum_options || @enum_map.keys
   end
 
   def self.type_name
@@ -19,5 +24,13 @@ class Apidef::EnumType < Apidef::BaseType
     options = @enum_options.join(', ')
 
     ["%s must be one of: #{options}"]
+  end
+
+  def raw_to_final_value(attr, raw_value, processor)
+    if @enum_map
+      @enum_map.fetch(raw_value)
+    else
+      raw_value
+    end
   end
 end
