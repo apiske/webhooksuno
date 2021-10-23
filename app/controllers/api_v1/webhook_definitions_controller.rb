171ee47a3aaa983d1dc210006fb479ba5968e458
André Diego Piske <andrepiske@gmail.com>
2021-10-23 09:53:19 +0200

    Change WebhookDefinitionsController to use Apidef

--- app/controllers/api_v1/webhook_definitions_controller.rb
+++ app/controllers/api_v1/webhook_definitions_controller.rb
@@ -1,17 +1,8 @@
 # frozen_string_literal: true
 
 class ApiV1::WebhookDefinitionsController < ApiController
-  class WebhookDefinitionContract < Wh::Contracts
-    schema do
-      optional(:name).value(:string)
-      optional(:description).value(:string)
-      # optional(:retry_policy)
-    end
-
-    rule(:name).validate(:entity_name)
-  end
-
   before_action :fetch_webhook_definition, only: [:show, :update]
+  before_action :process_input, only: [:update, :create]
 
   def index
     render_collection(@workspace.webhook_definitions.order(name: :asc).all)
@@ -22,33 +13,25 @@ class ApiV1::WebhookDefinitionsController < ApiController
   end
 
   def create
-    attributes = WebhookDefinitionContract.new.call(body_obj["data"].symbolize_keys)
-    return fail_validation!(attributes.errors) if attributes.failure?
+    @webhook_definition = WebhookDefinition.new
+    @webhook_definition.attributes = @processor.values_for_model
+    @webhook_definition.workspace = @workspace
 
-    webhook_definition = WebhookDefinition.new
-    set_webhook_definition_attributes(webhook_definition, attributes)
-    webhook_definition.workspace = @workspace
-    webhook_definition.save!
+    return unless with_common_record_checks do
+      @webhook_definition.save!
+    end
 
-    render status: :created, json: {
-      data: {
-        id: webhook_definition.public_uuid
-      }
-    }
+    render_single(@webhook_definition, :created)
   end
 
   def update
-    attributes = WebhookDefinitionContract.new.call(body_obj["data"].symbolize_keys)
-    return fail_validation!(attributes.errors) if attributes.failure?
+    @webhook_definition.attributes = @processor.values_for_model
 
-    set_webhook_definition_attributes(@webhook_definition, attributes)
-    @webhook_definition.save!
+    return unless with_common_record_checks do
+      @webhook_definition.save!
+    end
 
-    render status: :created, json: {
-      data: {
-        id: @webhook_definition.public_uuid
-      }
-    }
+    render_single(@webhook_definition)
   end
 
   private
@@ -58,17 +41,7 @@ class ApiV1::WebhookDefinitionsController < ApiController
     @webhook_definition = @workspace.webhook_definitions.find_by!(public_id: uuid)
   end
 
-  def set_webhook_definition_attributes(obj, data)
-    if data.key?(:name)
-      obj.name = data[:name]
-    end
-
-    if data.key?(:description)
-      obj.description = data[:description]
-    end
-
-    # if data.key?(:retry_policy)
-    #   obj.retry_policy = data[:retry_policy]
-    # end
+  def processor_entity_name
+    :webhook_definition
   end
 end
