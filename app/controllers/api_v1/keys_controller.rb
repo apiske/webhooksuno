e9a0d45d05e5a3049b1e41029e3399757b5881bb
André Diego Piske <andrepiske@gmail.com>
2021-11-05 22:30:36 +0100

    Implement key length validation

--- app/controllers/api_v1/keys_controller.rb
+++ app/controllers/api_v1/keys_controller.rb
@@ -4,6 +4,12 @@ class ApiV1::KeysController < ApiController
   before_action :fetch_key, only: [:show, :update]
   before_action :process_input, only: [:update, :create]
 
+  KEY_SIZES = {
+    hmac_sha1: 64,
+    hmac_sha256: 64,
+    hmac_sha512: 128
+  }.freeze
+
   def index
     render_collection(@workspace.keys.order(name: :asc).all)
   end
@@ -12,10 +18,30 @@ class ApiV1::KeysController < ApiController
     render_single(@key)
   end
 
+  def validate_key_content_size!
+    return false unless @key.content_changed? || @key.kind_changed?
+
+    key_kind_name = Key::Kind.r[@key.kind]
+    expected_size = KEY_SIZES.fetch(key_kind_name)
+
+    if @key.content.length != expected_size
+      @processor.add_error_fmt(@processor.api_definition.attrs['content'], nil,
+        "Expected a content length of exactly #{expected_size} bytes." +
+        " The key content length does not match the expected length of the key specified in the kind attribute." +
+        " If the key kind is hmac_sha1 or hmac_sha256, the content length must be exactly 64 bytes." +
+        " For hmac_sha512, the content length must be 128 bytes"
+      )
+    end
+
+    fail_on_invalid_processor!(@processor)
+  end
+
   def create
     @key = Key.new
     @key.attributes = @processor.values_for_model
     @key.workspace = @workspace
+
+    return if validate_key_content_size!
 
     return unless with_common_record_checks do
       @key.save!
@@ -25,26 +51,15 @@ class ApiV1::KeysController < ApiController
   end
 
   def update
-    return if fail_on_invalid_body_payload!
+    @key.attributes = @processor.values_for_model
 
-    processor = Apidef::Processor.new(:update, api_ctx, :key, ref_solver)
-    processor.process_input(body_obj["data"])
-
-    return if fail_on_invalid_processor!(processor)
-
-    key = Key.new
-    key.attributes = processor.values_for_model
-    key.workspace = @workspace
+    return if validate_key_content_size!
 
     return unless with_common_record_checks do
-      key.save!
+      @key.save!
     end
 
-    render status: :created, json: {
-      data: {
-        id: key.public_uuid
-      }
-    }
+    render_single(@key)
   end
 
   private
