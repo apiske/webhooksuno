d1e61f98ea6f5953a34ca3a0f10a38da137fb48e
André Diego Piske <andrepiske@gmail.com>
2022-11-29 21:38:33 -0300

    Reduce likelyhood of name clashing

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -5,7 +5,7 @@ end
 
 FactoryBot.define do
   trait :is_public do
-    name { FFaker::Internet.domain_word }
+    name { "#{FFaker::Internet.domain_word}_#{SecureRandom.alphanumeric(7)}" }
   #   public_id { UuidUtil.uuid_s_to_bin(SecureRandom.uuid) }
   end
 end
